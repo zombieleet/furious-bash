9c8090b6384010016a8626a2167ee522caaacebb
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-02-17 20:57:07 +0100

    support for plucking of function if their faster

--- benchmark.sh
+++ benchmark.sh
@@ -1,19 +1,11 @@
 #/usr/bin/env bash
 #set -x
-declare -A BENCHMARKS
 
-
-    
-
-time=$(which time)
-[[ $status -gt 0 ]] && {
-    printf "this program depends on the GNU/Linux time command"
-    exit 1;
-}
 bMark() {
     local subComm=$1
-
     
+    declare -gA temp_array;
+    declare -gA BENCHMARKS
 
     case $subComm in
 	add)
@@ -28,9 +20,38 @@ bMark() {
 	    }
 	    
 	    BENCHMARKS[$nameOfBenchMark]+="${addedFunction}"
+	    
+	    ;;
+	run)
+	    local OLDIFS="${IFS}"
+	    
+	    for getFunc in ${!BENCHMARKS[@]};do
+		
+		local __timeTaken=$(time ( ${BENCHMARKS[$getFunc]} ) 2>&1 1>/dev/null);
+		
+		IFS="\n"
+		
+		local timeTaken=$(sed -n '/real/s/real\s\+//p' <<<"${__timeTaken}");
+		
+		temp_array["${getFunc}"]="${BENCHMARKS[$getFunc]}(${getFunc}) ${timeTaken}"
 
-	;;
+		IFS=${OLDIFS}
+	    done
+	    
+	    declare -g __RUN=1;
+	    
+	    ;;
+	
 	complete)
+
+
+	    (( ! __RUN )) && {
+		printf "You have to call the run subcommand before calling ${subComm}\n"
+		return 1;
+	    }
+
+	    
+	    local timeTaken;
 	    local executeFunction=$2
 	    shift
 	    
@@ -38,25 +59,83 @@ bMark() {
 		printf "invalid number of arguments"
 		return 1;
 	    }
-	    
 	    declare -F $executeFunction &>/dev/null
 	    local status=$?
-
+	    
 	    (( status != 0 )) && {
 
 	    	printf "${executeFunction} has not been declared"
 		return 1;
 	    }
 	    
-	    for getFunc in ${!BENCHMARKS[@]};do
-		$time "%C ${BENCHMARKS[$getFunc]}"
-	    done
 	    
-	    #$executeFunction BENCHMARKS
-	    #return 0;
+	    $executeFunction "${temp_array[@]}"
+	    return 0;
 	    ;;
 	pluck)
 
+	    
+	    (( ! __RUN )) && {
+		printf "You have to call the run subcommand before calling ${subComm}\n"
+		return 1;
+	    }
+	    
+	    local extractType="${2}"
+	    
+	    shift
+	    
+	    [[ ! ${#} -eq 1 ]] && {
+		printf "invalid number of arguments\n"
+		return 1;
+	    }
+	    
+	    case $extractType in
+		fastest)
+		    local pushTimeRun
+		    
+		    for i in "${temp_array[@]}";do
+			local timeRun=${i} ; timeRun="${i##*)}"; timeRun="${timeRun//[ ms]/}"
+			pushTimeRun+=( ${timeRun} );
+		    done
+
+
+		    local result;
+		    
+		    for i in "${pushTimeRun[@]}";do
+			echo -e "\n\n$i -- master\n\n"
+			for j in "${pushTimeRun[@]}";do
+			    echo "$j -- slave"
+			    sleep 2
+			    local __bcResult=$( bc <<<"${i} <= ${j}" );
+			    (( __bcResult )) && {
+				for x in "${temp_array[@]}";do
+				    timeRun=${x} ; timeRun="${i##*)}"
+				    
+				    if [[ ${i} =~ ${timeRun} ]];then
+					
+					local nameOfRunner="${x}"
+					
+					nameOfRunner="${nameOfRunner%%)*}"; nameOfRunner="${nameOfRunner##*(}"
+					
+					timeRun="${x##*)}"
+					
+					#result="${timeRun} ${nameOfRunner}"
+					result="${nameOfRunner}"
+				    fi
+				done
+			    }
+			done
+
+		    done
+
+		    echo $result
+		    
+		;;
+		slowest)
+		;;
+		*)
+		    printf "Invalid type to extract options allowed are [fastest | slowest]\n"
+	    esac
 	    ;;
     esac
 }
@@ -64,16 +143,34 @@ bMark() {
 
 
 a() {
-    echo "hi"
+    for i in `seq 1 1000`;do
+	echo $i;
+    done    
+
 }
 c() {
-    echo "Hello world"
+
+    for i in {1..1000};do
+	echo $i
+    done
 }
 d() {
-    echo "montegro"
+    for ((i=0;i<=1000;i=i+1)) {
+	    echo $i;
+	}
 }
-bMark add a 'recurse'
-bMark add c 'tco'
-bMark add d 'nontco'
+cc() {
+    for i ;do
+	echo $i
+    done
+}
 
-bMark complete a
+bMark add c 'seq'
+bMark add d 'c'
+bMark add a 'builtin'
+
+bMark run
+bMark complete cc
+
+bMark pluck 'fastest'
+
