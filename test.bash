340e9c115198f8927b5e1a7d32175222a689e5b9
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-02-18 06:35:12 +0100

    unit testing for furious

--- test.bash
+++ test.bash
@@ -1,1 +1,16 @@
-printf "%s\n" "This is a test file"
+source ../assert-unit/testify.bash
+source benchmark.sh
+
+
+
+#bMark add seqLoop "seq"
+#bMark add cstyleLoop "cstyle"
+#bMark add builtinLoop "builtin"
+
+bMark add builtinList "builtin"
+bMark add lsStyle "lsstyle"
+
+bMark run
+bMark complete cc
+
+assert expect "$(bMark pluck 'fastest')" "lsstyle" "Test for Fastest Runner" "This should pass"
