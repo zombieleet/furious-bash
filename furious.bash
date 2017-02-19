e0964c34747f1146a13b5a7a96dc120315176990
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-02-19 07:38:08 +0100

    rename of bMark function to furious

--- furious.bash
+++ furious.bash
@@ -2,77 +2,77 @@
 #set -x
 
 extract() {
-    
+
     local op="${1}"
     local pushTimeRun
-    
+
     for i in "${temp_array[@]}";do
 	local timeRun=${i} ; timeRun="${i##*)}"; timeRun="${timeRun//[ ms]/}"
 	pushTimeRun+=( ${timeRun} );
     done
-    
-    
+
+
     local result;
-    
+
     for i in "${pushTimeRun[@]}";do
-	
+
 	for j in "${pushTimeRun[@]}";do
-	    
+
 	    [[ $i == $j ]] && continue ;
-	    
+
 	    local __bcResult=$( bc <<<"${i} $op ${j}" );
-	    
+
 	    (( __bcResult )) && {
-		
-		
+
+
 		[[ ! -z  "${res}" ]] && {
-		    
+
 		    local __test=$(bc <<<"${res} $op= ${i}")
-		    
+
 		    (( __test )) && {
 			break;
 		    }
 		}
-		
+
 		local res="$i"
-		
+
 		for x in "${temp_array[@]}";do
-		    
+
 		    timeRun=${x} ; timeRun="${timeRun##*)}" ; timeRun="${timeRun//[ms]/}"
-		    
+
 		    local __final=$( bc <<<"${res} == ${timeRun}" )
-		    
+
 		    (( __final )) && {
-			
+
 			local nameOfRunner="${x}"
-			
+
 			nameOfRunner="${nameOfRunner%%)*}"; nameOfRunner="${nameOfRunner##*(}"
-			
+
 			timeRun="${x##*)}"
-			
+
 			#result="${timeRun} ${nameOfRunner}"
 			result="${nameOfRunner}"
 		    }
 		done
-		
-		
-		
+
+
+
 	    } || {
 		break;
 	    }
 	done
-	
+
     done
-    
+
     echo $result
     #echo $res
-    
+
 }
 
 
-bMark() {
+furious() {
     local subComm=$1
-    
+
     declare -gA temp_array;
     declare -gA BENCHMARKS
 
@@ -80,37 +80,37 @@ bMark() {
 	add)
 	    local addedFunction=$2
 	    local nameOfBenchMark=$3
-	    
+
 	    shift #remove the first argument which is subComm
-	    
+
 	    [[ ! ${#} -eq 2 ]] && {
 		printf "invalid number of arguments"
 		return 1;
 	    }
-	    
+
 	    BENCHMARKS[$nameOfBenchMark]+="${addedFunction}"
-	    
+
 	    ;;
 	run)
 	    local OLDIFS="${IFS}"
-	    
+
 	    for getFunc in ${!BENCHMARKS[@]};do
-		
+
 		local __timeTaken=$(time ( ${BENCHMARKS[$getFunc]} ) 2>&1 1>/dev/null);
-		
+
 		IFS="\n"
-		
+
 		local timeTaken=$(sed -n '/real/s/real\s\+//p' <<<"${__timeTaken}");
-		
+
 		temp_array["${getFunc}"]="${BENCHMARKS[$getFunc]}(${getFunc}) ${timeTaken}"
 
 		IFS=${OLDIFS}
 	    done
-	    
+
 	    declare -g __RUN=1;
-	    
+
 	    ;;
-	
+
 	complete)
 
 
@@ -119,45 +119,45 @@ bMark() {
 		return 1;
 	    }
 
-	    
+
 	    local timeTaken;
 	    local executeFunction=$2
 	    shift
-	    
+
 	    [[ ! ${#} -eq 1 ]] && {
 		printf "invalid number of arguments"
 		return 1;
 	    }
 	    declare -F $executeFunction &>/dev/null
 	    local status=$?
-	    
+
 	    (( status != 0 )) && {
 
 	    	printf "${executeFunction} has not been declared"
 		return 1;
 	    }
-	    
-	    
+
+
 	    $executeFunction "${temp_array[@]}"
 	    return 0;
 	    ;;
 	pluck)
 
-	    
+
 	    (( ! __RUN )) && {
 		printf "You have to call the run subcommand before calling ${subComm}\n"
 		return 1;
 	    }
-	    
+
 	    local extractType="${2}"
-	    
+
 	    shift
-	    
+
 	    [[ ! ${#} -eq 1 ]] && {
 		printf "invalid number of arguments\n"
 		return 1;
 	    }
-	    
+
 	    case $extractType in
 		fastest)
 		    extract "<"
@@ -165,7 +165,7 @@ bMark() {
 		slowest)
 		    extract ">"
 		;;
-		
+
 		*)
 		    printf "Invalid type to extract options allowed are [fastest | slowest]\n"
 	    esac
@@ -176,4 +176,3 @@ bMark() {
 	    ;;
     esac
 }
-
