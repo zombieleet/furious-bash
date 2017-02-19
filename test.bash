e0bc16d3f110122e6e3babb9ea638683555667bf
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-02-19 06:45:04 +0100

    add functions to test

--- test.bash
+++ test.bash
@@ -1,19 +1,78 @@
-source ../assert-unit/testify.bash
-source furious.sh
+source ./testify.bash
+source ./furious.sh
 
 
 
-#bMark add seqLoop "seq"
-#bMark add cstyleLoop "cstyle"
-#bMark add builtinLoop "builtin"
+
+
+
+seqLoop() {
+    for i in `seq 1 1000`;do
+	echo $i;
+    done    
+    
+}
+cstyleLoop() {
+    for ((i=0;i<=1000;i=i+1)) {
+	    echo $i;
+	}
+	
+}
+builtinLoop() {
+    
+    for i in {1..1000};do
+	echo $i
+    done
+}
+
+cc() {
+    for i ;do
+	echo $i
+    done
+}
+
+
+builtinList() {
+    for i in *;do
+	echo $i
+    done
+}
+
+lsStyle() {
+    for i in $(ls);do
+	echo $i
+    done
+}
+
+
+bMark add seqLoop "seq"
+bMark add cstyleLoop "cstyle"
+bMark add builtinLoop "builtin"
+
+bMark run
+bMark complete cc
+printf "slowest  "
+bMark pluck slowest
+
+printf "fastest "
+bMark pluck fastest
+
+bMark unset
 
 bMark add builtinList "builtin"
 bMark add lsStyle "lsstyle"
 
 bMark run
-#bMark complete cc
 
-assert expect "$(bMark pluck 'fastest')" "lsstyle" "Test for Fastest Runner" "This should fail"
+bMark complete cc
 
-assert expect "$(bMark pluck 'fastest')" "builtin" "Test for Fastest Runner" "This test should pass"
-assert done
+printf "slowest  "
+bMark pluck slowest
+
+printf "fastest  "
+bMark pluck fastest
+
+#assert expect "$(bMark pluck 'fastest')" "lsstyle" "Test for Fastest Runner" "This should fail"
+#assert expect "$(bMark pluck 'fastest')" "builtin" "Test for Fastest Runner" "This test should pass"
+
+#assert done
